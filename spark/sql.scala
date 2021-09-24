04b6bbfbe5e3494838873cf4b84c5e27e8c7e412
Yannis Paetzelt <y.j.paetzelt@gmail.com>
2021-09-24 16:18:30 +0200

    tasks done?

--- spark/sql.scala
+++ spark/sql.scala
@@ -1,21 +1,23 @@
 import org.apache.spark.rdd._
 import org.apache.spark.sql.DataFrame 
-
+import org.apache.spark.sql.functions.typedLit
 
 object SQL {
   
   def minMaxAvg(df: DataFrame) = {
-    df.select(min("size").alias("min"), max("size").alias("max"), avg("size").alias("avg")).show
+    df.select(min("size").alias("min_size"),
+      max("size").alias("max_size"),
+      avg("size").alias("avg_size"))
   }
 
   def largestPop(df: DataFrame) = {
-    
-    
     df.createOrReplaceTempView("df_view")
 
-    spark.sql("""select * from df_view 
+    spark.sql("""
+      select * from df_view 
       where size = (select max(size) from df_view) 
-      order by hits""").show
+      order by hits
+      """).show
   }
   
   def sizeGTAvg(df: DataFrame) = {
@@ -23,8 +25,22 @@ object SQL {
     mma.createOrReplaceTempView("m_view")
     df.createOrReplaceTempView("df_view")
 
-    spark.sql("""select * from df_view
-      where size > (select avg from m_view) 
+    spark.sql("""
+      select * from df_view
+      where size > (select avg_size from m_view)
       """).show
+  }
+
+  def countDistinctTerms(df: DataFrame) = {
+    df.flatMap(f => f.getString(1).toLowerCase.split("_")).distinct.count
+  }
+
+  def mostFreqTerm(df: DataFrame) = {
+    df.flatMap(f => f.getString(1).toLowerCase.split("_"))
+      .map(t => (t,1))
+      .groupBy("_1")
+      .sum("_2")
+      .orderBy(desc("sum(_2)"))
+      .first
   }
 }
