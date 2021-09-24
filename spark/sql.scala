7a8b3e22492ab5f4e0a58365d9234ea1d64c7e1d
Yannis Paetzelt <y.j.paetzelt@gmail.com>
2021-09-24 15:35:03 +0200

    5

--- spark/sql.scala
+++ spark/sql.scala
@@ -2,17 +2,29 @@ import org.apache.spark.rdd._
 import org.apache.spark.sql.DataFrame 
 
 
-
-
 object SQL {
   
   def minMaxAvg(df: DataFrame) = {
-    df.select(min("size"), max("size"), avg("size")).show()
+    df.select(min("size").alias("min"), max("size").alias("max"), avg("size").alias("avg")).show
   }
 
   def largestPop(df: DataFrame) = {
-    val maxPop = df.select(max("hits"))
-    df.select(max(""))
+    
+    
+    df.createOrReplaceTempView("df_view")
+
+    spark.sql("""select * from df_view 
+      where size = (select max(size) from df_view) 
+      order by hits""").show
   }
   
+  def sizeGTAvg(df: DataFrame) = {
+    val mma = SQL.minMaxAvg(df)
+    mma.createOrReplaceTempView("m_view")
+    df.createOrReplaceTempView("df_view")
+
+    spark.sql("""select * from df_view
+      where size > (select avg from m_view) 
+      """).show
+  }
 }
